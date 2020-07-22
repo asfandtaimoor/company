8cc1b0299677a63e0b76875c38c33021529b2694
TaimoorSalyhal <asfandtaimoor786@gmail.com>
2020-07-22 20:33:13 +0500

    Make header Responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,14 @@ import { CoreModule } from './core/core.module';
 import { Home2Component } from './home2/home2.component';
 import { BlogListComponent } from './blog-list/blog-list.component';
 import { BlogDetailComponent } from './blog-detail/blog-detail.component';
+import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -32,9 +40,13 @@ import { BlogDetailComponent } from './blog-detail/blog-detail.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    NgZorroAntdModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
+
+
+
 export class HeaderComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  showMenu(){
+  document.getElementById('navLinks').style.display='block';
+  }
+
+  hideMenu(){
+    document.getElementById('navLinks').style.display='none'; 
+    }
 
 }
